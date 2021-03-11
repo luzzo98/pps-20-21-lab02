45c931cd1e8164212399dfeace9e7601e1dc065d
Simone Luzi <simone.luzi@studio.unibo.it>
2021-03-11 23:35:17 +0100

    removed old assetion in a class

--- src/Exercise/FunctionalComposition.scala
+++ src/Exercise/FunctionalComposition.scala
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.Assertions._
 object FunctionalComposition {
 
   def compose(f: Int => Int, g: Int => Int): Int => Int = {
-    (t) => f(g(t))
+    t => f(g(t))
   }
 
-  assertEquals(9,compose(_-1,_*2)(5))
-  assertEquals(4,compose(_+4,_%2)(10))
-
   def genericCompose[A,B,C](f: A => B, g: C => A): C => B = {
-    (t) => f(g(t))
+    t => f(g(t))
   }
 }
